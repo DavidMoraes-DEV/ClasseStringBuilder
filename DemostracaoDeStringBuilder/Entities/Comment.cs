533d0c6cd9fa111d27eff52cf9bc4a4227eb02e2
David Moraes <investtraderrs@gmail.com>
2020-09-22 10:32:48 -0300

    Exercicio StringBuilder

---
+++ DemostracaoDeStringBuilder/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace DemostracaoDeStringBuilder
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
